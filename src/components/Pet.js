51e2de89c95423c95141c7474a97c51de2099840
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-07-14 16:47:28 +0200

    remove state

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,15 +1,11 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 export default function Pet(props) {
   const { pet } = props
-  const [fav, setFav] = useState(false)
-
-  // 1- Add a click handler to the button that favs the pet
 
   return (
     <div className='pet-friends container'>
       <h4>{pet.name} ({pet.type})</h4>
-      {fav ? '❤️' : <button>fav</button>}
     </div>
   )
 }
