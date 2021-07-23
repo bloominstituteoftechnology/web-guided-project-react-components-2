0070593ffeaa8f7fe8f6eee4c0232eeadd95757c
Timothy Roy <timothy.roy@protonmail.ch>
2021-07-22 22:04:25 -0500

    Use friend data to render actual friend information

--- src/components/Friend.js
+++ src/components/Friend.js
@@ -3,19 +3,22 @@ import PetsList from './PetsList'
 
 export default function Friend(props) {
   // 👉 1- What does a Friend need?
+  // destructure props and pull out the variable we want
+  const { friend } = props
 
   return (
     <div className='friend-friends container'>
       {/* 👉 2- Fix the JSX so it displays real info coming into the component */}
       <div className='friend-info'>
         <div >
-          <h3>Name: Jessica</h3>
-          <p>Age: 26</p>
+          <h3>Name: {friend.name}</h3>
+          <p>Age: {friend.age}</p>
 
-          <p>Married: yes <button>change</button></p>
+          <p>Married: {friend.married ? 'yes' : 'no'} <button>change</button></p>
           <div>Likes:
             <ul>
               {/* 👉 3- Loop over the friend's hobbies and generate <li/> elements as you go */}
+              { friend.hobbies.map(hobby => <li> {hobby} </li> ) }
             </ul>
           </div>
         </div>
