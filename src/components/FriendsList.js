1fb3c38a5e5791c78d63ec013e9776b5af13bd3e
Timothy Roy <timothy.roy@protonmail.ch>
2021-07-22 22:03:57 -0500

    LDestructure to get friends variable, then loop over to create child components with Friend

--- src/components/FriendsList.js
+++ src/components/FriendsList.js
@@ -1,12 +1,25 @@
 import React from 'react'
 // 👉 1- FriendsList renders several Friend components, import Friend
+import Friend from './friend'
 
-export default function FriendsList(props) {
+export default function FriendsList({ friends }) {
+
   // 👉 2- What data does FriendsList need to do its job? Use destructuring
+  // use destructuring to pull the friends array out of props, as a variable
+
+  // const { friends } = props
+
+
+  // js arrays, .map vs .forEach? --> .map builds a new array
+  // [<Friend />, <Friend />, <Friend />, <Friend />]
+
   return (
     <div className='list-friends container'>
       {/* 👉 3- We need to loop over the data rendering a Friend as we go */}
       {/* Each friend is going to need a `key` prop and also some other prop with data */}
+
+      {friends.map(friendObj => <Friend key={friendObj.id} friend={friendObj} />)}
+
     </div>
   )
 }
