767d1d6b6375cd096304eb1d05570ee68bafae0a
Timothy Roy <timothy.roy@protonmail.ch>
2021-07-22 22:03:02 -0500

    Store data in App with useState, pass to child component FriendsList

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,25 @@
 // This is the top-level component
 // so we'll keep application state at this level.
-// 👉 1- Import the state hook!
-import React from 'react'
+// 👉 1- Import the useState hook!
+import React, { useState } from 'react'
 import FriendsList from './FriendsList'
 import Search from './Search'
 // 👉 2- Import the dummy data that will power the application.
 // (Tomorrow we'll fetch the data from an API instead.)
 
+// we use mock data all the time, so you can build a front-end component without an API
+// axios.get('https://mycompany.com/friends')
+import friendsMockData from '../dummy-data/friends'
 
 export default function App() {
   // 👉 3- Initialize a slice of state to keep track of the data
   // using the dummy data as the initial value of the slice of state
+  // how to use useState()??
+  // what does the function useState() return? --> 
+  // an array, with a value, and a function to change the value
+
+  // why use useState and the setFriends function??
+  const [friends, setFriends] = useState(friendsMockData)
 
   // 👉 4- Initialize a slice to keep track of the value of the search box
   // using an empty string as the initial value of the slice
@@ -27,7 +36,11 @@ export default function App() {
       {/* STRETCH - Changes to the input should update the search term */}
 
       {/* 👉 7- Render the FriendsList component */}
+      {/*  'render' here just means to invoke the component  */}
+      {/* sometimes components need data BEFORE you can render them  */}
       {/* What prop/props does FriendsList need? */}
+      <FriendsList friends={friends} />
+
     </div>
   )
 }
