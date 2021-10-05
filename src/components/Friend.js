d17a4edccca2f7d046a54258177f04b7217e2ffa
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-10-05 10:52:24 -0700

    update components

--- src/components/Friend.js
+++ src/components/Friend.js
@@ -3,19 +3,29 @@ import PetsList from './PetsList'
 
 export default function Friend(props) {
   // 👉 1- What does a Friend need?
+  const { friend, changeStatus } = props;
 
   return (
     <div className='friend-friends container'>
       {/* 👉 2- Fix the JSX so it displays real info coming into the component */}
       <div className='friend-info'>
         <div >
-          <h3>Name: Jessica</h3>
-          <p>Age: 26</p>
+          <h3>Name: {friend.name}</h3>
+          <p>Age: {friend.age} </p>
 
-          <p>Married: yes <button>change</button></p>
+          <p>Married: {friend.married ? 'Yes' : 'No'} <button onClick={() => changeStatus(friend.id)}>change</button></p>
           <div>Likes:
             <ul>
               {/* 👉 3- Loop over the friend's hobbies and generate <li/> elements as you go */}
+              {friend.hobbies.map((like, idx) => <li key={idx}>{like}</li>)}
+              {/**
+               * for (let i = 0; i < friend.hobbies.length; i++) {
+               *    const like = friend.hobbies[i];
+               *    const idx = i;
+               *    const liElem = document.createElement('li');
+               *    liElem.innerText = like;
+               * }
+               */}
             </ul>
           </div>
         </div>
@@ -24,7 +34,7 @@ export default function Friend(props) {
           {/* 👉 3- What data does the PetsList need? */}
           {/* What is the exact name of the prop/props it expects? */}
           {/* Is the data around here somewhere so I may pass it? */}
-          <PetsList />
+          <PetsList pets={friend.pets} />
         </div>
       </div>
     </div>
